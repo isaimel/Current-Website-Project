207be3f2c9b9e28055e5c77285f9a70c2ecc0c15
isaimel <isaimel001@gmail.com>
2026-05-21 21:48:34 -0400

    fixed json duplicate

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const queryURL = "https://raw.githubusercontent.com/isaimel/Current-Website-Project/refs/heads/main/lists.json?token=GHSAT0AAAAAAD5HAEFHFZJGDVPF2VAUN46E2QPWHYA";
-  const listsData = search(queryURL)
-  var saulList = listsData.saulList;
+  const queryURL = "https://raw.githubusercontent.com/isaimel/Current-Website-Project/refs/heads/main/lists.json?token=GHSAT0AAAAAAD5HAEFGGLPFEYELXMB6WMKG2QPWWAQ";
+
+  fetch(queryURL)
+    .then(response => response.json())
+    .then(listsData => {
+      const saulList = listsData.saulList;
+      const scrollContainer = document.getElementById("portfolio_scroll");
+      addScrollFunctionality(scrollContainer, saulList);
+    })
+    .catch(error => console.log('Error during fetch: ' + error.message));
+
 
   var scrollContainer = document.getElementById("portfolio_scroll");
-  addScrollFunctionality(scrollContainer)
+  addScrollFunctionality(scrollContainer, saulList);
   
-  function addScrollFunctionality(scrollContainer){
+  function addScrollFunctionality(scrollContainer, givenList){
     var leftDiv = scrollContainer.querySelector(".left_image");
     var leftImage = document.createElement("img");
     leftDiv.appendChild(leftImage);
@@ -25,15 +33,16 @@ document.addEventListener('DOMContentLoaded', () => {
     var centralIndex = 1;
     leftButton.addEventListener("click", () => plusDivs(-1));
     rightButton.addEventListener("click", () => plusDivs(1));
-    showDivs(1);
+    showDivs(centralIndex);
+
     function plusDivs(n) {
       showDivs(centralIndex += n);
     }
 
     function showDivs(n) {
-      leftImage.src = './' + saulList[modLoop(n-1, saulList.length)] + '.jpg';
-      centerImage.src = './' + saulList[modLoop(n, saulList.length)] + '.jpg';
-      rightImage.src = './' + saulList[modLoop(n+1, saulList.length)] + '.jpg';
+      leftImage.src = './' + givenList[modLoop(n-1, givenList.length)] + '.jpg';
+      centerImage.src = './' + givenList[modLoop(n, givenList.length)] + '.jpg';
+      rightImage.src = './' + givenList[modLoop(n+1, givenList.length)] + '.jpg';
     }
   }
 
@@ -42,22 +51,4 @@ document.addEventListener('DOMContentLoaded', () => {
     if (n < 0) {return cap - 1}
     return n;
   } 
-
-  function search(queryURL) {
-    fetch(queryURL)
-            .then(function (response) {
-                // response.json() returns a json string,
-                // returning it will convert it 
-                // to a pure JavaScript 
-                // object for the next then's callback
-                return response.json();
-            })
-            .then(function (users) {
-                // users is a JavaScript object here
-                displayUsersAsATable(users);
-            })
-            .catch(function (error) {
-                console.log('Error during fetch: ' + error.message);
-            });
-          }
 });
